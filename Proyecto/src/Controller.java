42fc674e9f34236480c6b4dcb40f7878ec6006f9
Anderson <231968621+Anderson-dx-0714@users.noreply.github.com>
2025-11-10 18:31:54 -0500

    Se creo la clase controladora y los metodos

---
+++ Proyecto/src/Controller.java
@@ -0,0 +1,13 @@
+
+public class Controller  {
+   public void agregarPracticante(){    
+   }
+   public void registrarAsistencia(){
+   }
+   public void buscarPracticante(){
+   }
+   public void mostrarAsistenciaPorfecha(){
+   }
+   public void obtenerReporte(){
+   }   
+}

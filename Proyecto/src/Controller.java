39e03d6eb508bd81cbf43b61565def03bec52397
Jasmin Alcántara <205731838+Jasmin-Git-Hub@users.noreply.github.com>
2025-11-10 18:40:31 -0500

    Se implmentó el método agregarPracticante

--- Proyecto/src/Controller.java
+++ Proyecto/src/Controller.java
@@ -1,13 +1,22 @@
 
+import java.util.ArrayList;
+
+
 public class Controller  {
-   public void agregarPracticante(){    
+    private ArrayList<Practiicante> practicantes = new ArrayList(); 
+   public void agregarPracticante(Practiicante p){ 
+       practicantes.add(p); 
    }
    public void registrarAsistencia(){
+       
    }
    public void buscarPracticante(){
+       
    }
    public void mostrarAsistenciaPorfecha(){
+       
    }
    public void obtenerReporte(){
+       
    }   
 }

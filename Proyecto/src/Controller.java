ed3ad53737d22f4ebe0ff8666f7801ecd105dd98
israelAja <205737875+israelAja@users.noreply.github.com>
2025-11-10 19:02:15 -0500

    se implmento el metodo registrar asistencia

--- Proyecto/src/Controller.java
+++ Proyecto/src/Controller.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 
 public class Controller  {
     private ArrayList<Practiicante> practicantes = new ArrayList(); 
+    private ArrayList<Asistencia> asistencia=new ArrayList();
    public void agregarPracticante(Practiicante p){ 
        practicantes.add(p); 
    }
-   public void registrarAsistencia(){
-       
+   public void registrarAsistencia(Asistencia a){
+       asistencia.add(a);
+       System.out.println("asistencia registrada para "+practicantes.get);
    }
    public void buscarPracticante(){
        

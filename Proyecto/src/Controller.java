003ad4ffeacea03bf0460aeddc23bc63519bb335
Jasmin Alcántara <205731838+Jasmin-Git-Hub@users.noreply.github.com>
2025-11-18 11:54:55 -0500

    Se implementarón cambios en Controller

--- Proyecto/src/Controller.java
+++ Proyecto/src/Controller.java
@@ -2,17 +2,20 @@
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 
 public class Controller  {
     private ArrayList<Practiicante> practicantes = new ArrayList(); 
     private ArrayList<Asistencia> asistencias=new ArrayList();
     private ArrayList<Actividad> actividad=new ArrayList();
+    private ArrayList<Insidencia> incidencia=new ArrayList();
+    
     private ArrayList<Profesor> profesor=new ArrayList();
     private ArrayList<Administrador> administrador=new ArrayList();
     private ArrayList<Secretaria> secretaria=new ArrayList();
     private ArrayList<Practiicante> practicante=new ArrayList();
-    private ArrayList<Insidencia> incidencia=new ArrayList();
      
     private Persona usuarioLogeado =null;
     public Controller(){
@@ -28,16 +31,15 @@ public class Controller  {
         p2.setProfesorAsignado(profesor.get(0));
         practicantes.add(p2);
     }
-    
-    
+  
    public String agregarPracticante(int id, String nombre, String ap, String dni, String carrera, String area, String email, String contraseña){ 
        if (!(usuarioLogeado instanceof Administrador)) return "ERROR: Sin permisos.";
        if (buscarPracticante(dni) != null) return "ERROR: Ya existee un practicante con ese DNI.";
        Practiicante p1 = new Practiicante(id, nombre, ap, dni, carrera, area, email, contraseña);
        practicantes.add(p1); 
        return "Practicante agregado.";
-       
    }
+   
    public String registrarAsistencia(String dniPracticante, String estado){
        if(!(usuarioLogeado instanceof Secretaria)&&!(usuarioLogeado instanceof Administrador)){
            return "ERROR: No tiene permisos para esta acción"; 
@@ -56,9 +58,8 @@ public class Controller  {
        Asistencia  nuevaAsistencia = new Asistencia(idAsistencia, hoy, hora, "", estado, p);
        asistencias.add(nuevaAsistencia);
        return "Asistencia registrada conrrectamente.";
-       
-       
    }
+   
    public Practiicante buscarPracticante(String dni){
        for (Practiicante p : practicantes) {
            if (p.getDni().equals(dni)) {
@@ -66,6 +67,7 @@ public class Controller  {
            }
        } return null;
    }
+   
    public ArrayList<Asistencia>mostrarAsistenciaPorfecha(LocalDate fecha){
        ArrayList<Asistencia>listaFiltrada=new ArrayList<>();
        for (Asistencia a : asistencias) {
@@ -75,6 +77,7 @@ public class Controller  {
        }
        return listaFiltrada;
    }
+   
    public void obtenerReporte(){
        System.out.println("Reporte generado:");
        for (Asistencia a : asistencias) {
@@ -109,12 +112,15 @@ public class Controller  {
        } 
        return false;
    }
+   
    public void cerrarSesion(){
        this.usuarioLogeado = null; 
    }
+   
    public Persona getUsuarioLogueado(){
        return this.usuarioLogeado; 
    }
+   
    public String registrarActividad(String dniPracticante, String descripcion){
        if(!(usuarioLogeado instanceof Profesor)) return "ERROR: solo los profesores pueden registrar observaciones"; 
        Practiicante p = buscarPracticante(dniPracticante); 
@@ -123,8 +129,9 @@ public class Controller  {
        Actividad act = new Actividad(id, LocalDate.now(), descripcion, (Profesor)usuarioLogeado); 
        actividad.add(act); 
        p.agregarActividad(act);
-       return "Observación registrada"; 
+       return "Actividad registrada"; 
    }
+   
    public String modificarActividad(int idAct, String nuevoTexto){
        if(!(usuarioLogeado instanceof Profesor)) return "ERROR: Sin permisos"; 
        for (Actividad act : actividad) {
@@ -138,7 +145,7 @@ public class Controller  {
                }
            }
        }
-       return "ERROR: Activida no encontrada"; 
+       return "ERROR: Actividad no encontrada"; 
    }
    
    public String desactivarPracticante(String dni){
@@ -172,10 +179,8 @@ public class Controller  {
                ausentesMarcados++;
            }
        } return "se marcaron" + ausentesMarcados + "Ausencias.";
-       
-   
-      
    }
+   
    public String modificarPracticante(String dni, String nuevoNombre, String nuevaCarrera){
       if (!(usuarioLogeado instanceof Administrador)) return "ERROR: Sin permisos.";
         
@@ -185,7 +190,66 @@ public class Controller  {
         p.setNombre(nuevoNombre);
         p.setCarrera(nuevaCarrera);
         return "Practicante modificado.";
-       
    }
    
+   public ArrayList<Practiicante> practicantesActivos(){
+       ArrayList<Practiicante> activo =new ArrayList<>(); 
+       for (Practiicante p : practicantes) {
+           if(p.isActivo()) activo.add(p); 
+       }
+       return activo; 
+   }
+   
+   public ArrayList<Practiicante> practicantesAsignados(){
+       ArrayList<Practiicante> asignado = new ArrayList<>();
+       if (!(usuarioLogeado instanceof Profesor)) return asignado; 
+       Profesor profLogueado = (Profesor) usuarioLogeado; 
+       for (Practiicante p : practicantes) {
+           if(p.getProfesorAsignado()!=null&&p.getProfesorAsignado().equals(profLogueado)){
+               asignado.add(p); 
+           }
+       }
+       return  asignado; 
+   }
+   
+   public ArrayList<Asistencia> historialAsistencia(String dni){
+       ArrayList<Asistencia> historial = new ArrayList<>(); 
+       for (Asistencia a : asistencias) {
+           if (a.getPracticante().getDni().equals(dni)){
+               historial.add(a); 
+           }
+       }
+       Collections.sort(historial, (Asistencia a1, Asistencia a2) -> a2.getFecha().compareTo(a1.getFecha())); 
+       return  historial;
+   }
+   
+   public ArrayList <Actividad> historialActividad(String dni, LocalDate inicio, LocalDate fin){
+       Practiicante p = buscarPracticante(dni); 
+       if (p==null) return new ArrayList<Actividad>(); 
+       ArrayList<Actividad> historialFiltrado = new ArrayList<>(); 
+       for (Actividad act : p.getActividad()) {
+           if((inicio==null||!act.getFecha().isBefore(inicio))&&(fin==null||!act.getFecha().isAfter(fin))){
+               historialFiltrado.add(act); 
+           }
+       }
+       return historialFiltrado; 
+   }
+   
+   public ArrayList<Asistencia> visualizarAsistencias(){
+       if(!(usuarioLogeado instanceof Practiicante)) return new ArrayList<Asistencia>();
+       return historialAsistencia(usuarioLogeado.getDni()); 
+   }
+   
+   public ArrayList<Actividad> visualizarActividades(){
+       if(!(usuarioLogeado instanceof Practiicante)) return new ArrayList<Actividad>(); 
+       return historialActividad(usuarioLogeado.getDni(), null, null); 
+   }
+   
+   public String reportarIncidencia(String mensaje){
+       if(!(usuarioLogeado instanceof Practiicante)) return "ERROR: Solo parcticantes pueden reportar."; 
+       int id = incidencia.size()+1; 
+       Insidencia inc = new Insidencia(id, LocalDate.now(), mensaje, (Practiicante) usuarioLogeado); 
+       incidencia.add(inc); 
+       return "Incidencia reportada. ID: "+ id; 
+   }
 }

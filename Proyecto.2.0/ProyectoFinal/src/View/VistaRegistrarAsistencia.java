5aad0bf9f868132eb2fb00ae79d1e6d3ad25c014
israelAja <205737875+israelAja@users.noreply.github.com>
2025-11-29 14:56:53 -0500

    nuevo cambio

--- Proyecto.2.0/ProyectoFinal/src/View/VistaRegistrarAsistencia.java
+++ Proyecto.2.0/ProyectoFinal/src/View/VistaRegistrarAsistencia.java
@@ -8,13 +8,21 @@ package View;
  *
  * @author pauls
  */
+import Controller.Controller;
 public class VistaRegistrarAsistencia extends javax.swing.JFrame {
-
+        private Controller controladora;
     /**
      * Creates new form VistaRegistrarAsistencia
      */
-    public VistaRegistrarAsistencia() {
+    public VistaRegistrarAsistencia(Controller controladora) {
         initComponents();
+        
+        cboEstado.removeAllItems();
+        cboEstado.addItem("Presente");
+        cboEstado.addItem("Tardanza");
+        cboEstado.addItem("Falta");
+        
+        setLocationRelativeTo(null);
     }
 
     /**
@@ -92,38 +100,7 @@ public class VistaRegistrarAsistencia extends javax.swing.JFrame {
 
     /**
      * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(VistaRegistrarAsistencia.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(VistaRegistrarAsistencia.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(VistaRegistrarAsistencia.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(VistaRegistrarAsistencia.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new VistaRegistrarAsistencia().setVisible(true);
-            }
-        });
-    }
+  
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnRegresarMenu;
